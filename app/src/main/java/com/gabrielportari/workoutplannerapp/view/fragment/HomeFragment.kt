c88ccc52ef359a6f437ab98ed1583262ae39c6eb
Gabriel Portari <portari15@gmail.com>
2024-12-16 17:55:51 -0300

    inicio da construção da tela home

--- app/src/main/java/com/gabrielportari/workoutplannerapp/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/gabrielportari/workoutplannerapp/view/fragment/HomeFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gabrielportari.workoutplannerapp.data.listener.SelectWorkoutListener
 import com.gabrielportari.workoutplannerapp.databinding.FragmentHomeBinding
+import com.gabrielportari.workoutplannerapp.view.adapter.HomeAdapter
 import com.gabrielportari.workoutplannerapp.viewmodel.HomeViewModel
 
 
@@ -17,14 +21,35 @@ class HomeFragment : Fragment() {
 
     private var userName = "user"
 
-    //private val adapter = HomeAdapter()
+    private val adapter = HomeAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.recyclerHome.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerHome.adapter = adapter
+
+        val listener = object: SelectWorkoutListener {
+            override fun onSelect(id: Int) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        adapter.attachListener(listener)
+
+        viewModel.loadUserName()
+        viewModel.listWorkouts()
+
+        observe()
         return binding.root
+    }
+
+    private fun observe(){
+
     }
 
     override fun onResume() {
--- app/src/main/java/com/gabrielportari/workoutplannerapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/gabrielportari/workoutplannerapp/viewmodel/HomeViewModel.kt
@@ -1,4 +1,29 @@
 package com.gabrielportari.workoutplannerapp.viewmodel
 
-class HomeViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.gabrielportari.workoutplannerapp.data.model.Validation
+import com.gabrielportari.workoutplannerapp.data.repository.UserRepository
+import com.gabrielportari.workoutplannerapp.data.repository.WeekRepository
+
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+    private val weekRepository = WeekRepository.getInstance(application.applicationContext)
+    private val userRepository = UserRepository.getInstance(application.applicationContext)
+
+
+    private val _validation = MutableLiveData<Validation>()
+    val validation: LiveData<Validation> get() = _validation
+
+    private val _userName = MutableLiveData<String>()
+    val userName: LiveData<String> get() = _userName
+
+    fun loadUserName(){
+        _userName.value = "User"
+    }
+
+    fun listWorkouts(){
+
+    }
 }
