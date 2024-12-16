9753e4bfa29849ea6a2f630f9dc6b70941dbd9b0
Gabriel Portari <portari15@gmail.com>
2024-12-16 17:40:48 -0300

    inicio da construção da tela home

---
+++ app/src/main/java/com/gabrielportari/workoutplannerapp/view/adapter/HomeAdapter.kt
@@ -0,0 +1,35 @@
+package com.gabrielportari.workoutplannerapp.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gabrielportari.workoutplannerapp.data.listener.SelectWorkoutListener
+import com.gabrielportari.workoutplannerapp.data.model.WorkoutDay
+import com.gabrielportari.workoutplannerapp.databinding.WorkoutOfDayItemBinding
+import com.gabrielportari.workoutplannerapp.view.viewholder.HomeViewHolder
+
+class HomeAdapter : RecyclerView.Adapter<HomeViewHolder>() {
+    private lateinit var listener: SelectWorkoutListener
+    private var workouts: List<WorkoutDay> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemBinding = WorkoutOfDayItemBinding.inflate(inflater, parent, false)
+        return HomeViewHolder(itemBinding, listener)
+
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        val actualPosition = position % workouts.size
+        val workout = workouts[actualPosition]
+        holder.bind(workout)
+    }
+
+    override fun getItemCount(): Int {
+        return Integer.MAX_VALUE
+    }
+
+    fun attachListener(workoutListener: SelectWorkoutListener) {
+        listener = workoutListener
+    }
+}
--- app/src/main/java/com/gabrielportari/workoutplannerapp/view/viewholder/HomeViewHolder.kt
+++ app/src/main/java/com/gabrielportari/workoutplannerapp/view/viewholder/HomeViewHolder.kt
@@ -1,24 +1,36 @@
 package com.gabrielportari.workoutplannerapp.view.viewholder
 
+import android.view.Gravity
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.gabrielportari.workoutplannerapp.data.constants.MyConstants
+import com.gabrielportari.workoutplannerapp.data.listener.SelectWorkoutListener
 import com.gabrielportari.workoutplannerapp.data.model.Workout
 import com.gabrielportari.workoutplannerapp.data.model.WorkoutDay
 import com.gabrielportari.workoutplannerapp.databinding.WorkoutOfDayItemBinding
 import java.util.Calendar
 
-class HomeViewHolder(private val itemBinding: WorkoutOfDayItemBinding, val listener: HomeListener):
+class HomeViewHolder(private val itemBinding: WorkoutOfDayItemBinding, val listener: SelectWorkoutListener):
 RecyclerView.ViewHolder(itemBinding.root){
 
     fun bind(workout: WorkoutDay){
         itemBinding.textDayOfWeek.text = workout.day
         itemBinding.textWorkoutOfDayName.text = workout.workout?.name
         itemBinding.textWorkoutOfDayDescription.text = workout.workout?.description
-        itemBinding.textIsToday.visibility = if(isToday(workout.day)) View.VISIBLE else View.GONE
+        if(isToday(workout.day)){
+            itemBinding.textIsToday.visibility = View.VISIBLE
+            itemBinding.layoutWorkoutDayItem.gravity = Gravity.TOP
+        }else{
+            itemBinding.textIsToday.visibility = View.GONE
+            itemBinding.layoutWorkoutDayItem.gravity = Gravity.CENTER
+        }
+
+        itemBinding.layoutWorkoutDayItem.setOnClickListener{
+            listener.onSelect(workout.workout!!.id)
+        }
     }
 
-    fun isToday(day: String) : Boolean{
+    private fun isToday(day: String) : Boolean{
         return when(day){
             MyConstants.WEEK_DAYS.MONDAY -> Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY
             MyConstants.WEEK_DAYS.TUESDAY -> Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == Calendar.TUESDAY
